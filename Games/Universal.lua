89405ed1cf51e82e75853fcf9bb27dc8d94fd19c
chienminh21 <hhffgg436@gmail.com>
2026-09-19 21:16:40 -0700

    Update Universal.lua

--- Games/Universal.lua
+++ Games/Universal.lua
@@ -1,1 +1,14 @@
+-- File: Games/Universal.lua
+local baseUrl = "https://raw.githubusercontent.com/chienminh21/PhiPhaiv3/main/"
+
+local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Footagesus/WindUI/main/dist/main.lua"))()
+local UIBuilder = loadstring(game:HttpGet(baseUrl .. "Core/UIBuilder.lua"))()
+
+local Window = WindUI:CreateWindow({
+    Title = "PhiPhai v3 | Universal",
+    Folder = "PPv3Save",
+    Size = UDim2.fromOffset(580, 420),
+})
+
+UIBuilder.Build(Window)
 
